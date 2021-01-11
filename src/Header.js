e68baf7ffcb106ca149728344990a0e29d8262f9
Jyoti Adhikari <jyotiad18@gmail.com>
2021-01-11 14:32:16 +0100

    remove unwanted code

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import './css/Header.css';
 import logo from './images/logo.png';
 import { useStateValue } from './StateProvider';
